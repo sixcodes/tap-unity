6b4df7e43c4a01aede6e1706d494b923f33f03f7
Gabriel Seixas <salesseixasgabriel@gmail.com>
2022-07-06 02:47:51 -0300

    changed structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-unity-2",
+    version="0.1.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_unity"],
+    install_requires=[
+        # NB: Pin these to a more specific version for tap reliability
+        "singer-python",
+        "requests",
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-unity-2=tap_unity:main
+    """,
+    packages=["tap_unity"],
+    package_data = {
+        "schemas": ["tap_unity/schemas/*.json"]
+    },
+    include_package_data=True,
+)

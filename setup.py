c16fff6775032f13dfb67eceb5a1aba8232b7900
Jesue Junior <jesuesousa@gmail.com>
2022-08-16 11:17:23 -0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-unity",
-    version="0.2.2"
+    version="0.2.3"
             "",
     description="Singer.io tap for extracting data from Unity API",
     author="Jesue Junior <jesuesousa@gmail.com>",
